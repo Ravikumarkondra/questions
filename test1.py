0fdb14b4c3582c3a9875c43dba5c1a330c29f3c9
username <ravikumarkondramarolix@gmail.com>
2023-11-22 14:09:06 +0530

    questions Adding

---
+++ test1.py
@@ -0,0 +1,11 @@
+# num = int(input("Enter a number: "))
+# print(str)
+# rev = int(input(str(num)[::-1]))
+# print(rev)
+
+
+# 2. str revers character
+str = input("Enter a string : ")
+print(str)
+ret = str[::-1]
+print(ret)
